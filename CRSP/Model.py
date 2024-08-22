367340aab0330c30f7e44a86e7166cc1298be399
katerina-anastasiadou <145467384+katerina-anastasiadou@users.noreply.github.com>
2024-08-22 10:14:02 +0100

    Update Model.py

--- CRSP/Model.py
+++ CRSP/Model.py
@@ -18,11 +18,7 @@ class MyModel:
         self.y = self.model_instance.binary_var_matrix(data_input.V, data_input.V, name='y')
         
 
-        # Objective function (3)
-        # self.model_instance.minimize(
-        #     self.model_instance.sum(self.x[i, j] * data_input.c[i, j] for (i,j) in data_input.E) +
-        #     self.model_instance.sum(self.y[i, j] * data_input.a[i, j] for (i,j) in data_input.A))
-        
+        # Objective function (3)        
         self.model_instance.minimize(
             self.model_instance.sum(self.x[i, j] * data_input.c[i, j] for (i,j) in data_input.E) +
             self.model_instance.sum(self.y[i, j] * data_input.d[i, j] for (i,j) in data_input.A))
@@ -46,33 +42,22 @@ class MyModel:
             self.y[0, j]  == 0
             for j in data_input.V if j !=0 )
         
-        # Ensure y[i, j] is less than or equal to y[j, j] for all i, j
-        # self.model_instance.add_constraints(self.y[i,j] <= self.y[j,j] for i in data_input.V for j in data_input.V)
-        
         # Additional constraint (12): x[i, j] + y[i, j] <= y[j, j] for all pairs of vertices i, j
         # where S = {v_i, v_j} and S \ {v_1} is true
         self.model_instance.add_constraints(
             self.x[e] + self.y[i, j] <= self.y[j, j] 
             for i in data_input.V for j in data_input.V for e in get_edges([i,j],data_input.E)
-            if i != 0 and j != 0 and i != j 
-        )
+            if i != 0 and j != 0 and i != j)
         
 
         # Constraint (13): x[0, i] <= y[i, i] for all vi in V \ {v1}
         self.model_instance.add_constraints(
-            self.x[e] <= self.y[i, i] for i in data_input.V for e in get_edges([0,i],data_input.E) if i != 0
-        )
+            self.x[e] <= self.y[i, i] for i in data_input.V for e in get_edges([0,i],data_input.E) if i != 0)
         
         
         # Capacity constraint
         # Cover inequality: Total demand assigned to j should not exceed Qmax * y[j, j]
         self.model_instance.add_constraints(
             self.model_instance.sum(data_input.demand[i] * self.y[i, j] for i in data_input.V) <= data_input.Qmax * self.y[j, j]
-            for j in data_input.V
-        )
-        
-        
-        
-        
-        
+            for j in data_input.V)
         
